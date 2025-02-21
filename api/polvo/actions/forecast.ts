61d9108b17537b436be7964976ed6248b344b455
mayack <me@mayack.com>
2025-02-21 17:47:17 +0000

    Pushed response header for getForecast.

--- api/polvo/actions/forecast.ts
+++ api/polvo/actions/forecast.ts
@@ -60,6 +60,12 @@ export async function getForecast(coords: {
       units,
       token
     )
+    // Set cache headers for SSR fallback (if ISR isn’t used)
+    const responseHeaders = headers()
+    responseHeaders.set(
+      'Cache-Control',
+      'public, s-maxage=600, stale-while-revalidate=300'
+    )
     return {
       data: forecast,
       error: null,
