ac32fa0139fefad2522c6d04c2a8c56a788af41e
mayack <me@mayack.com>
2025-03-02 17:41:52 +0000

    HotFix: v0.1.9
    Component rename.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 import { Spinner } from '@/components/ui/spinner'
 import React from 'react'
 
-export default function RootLoading(): React.JSX.Element {
+export default function Loading(): React.JSX.Element {
   return (
     <div className="fixed inset-0 z-[100] flex size-full flex-col items-center justify-center gap-4 bg-background">
       <Spinner size="lg" />
