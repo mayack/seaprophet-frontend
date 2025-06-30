93478c2924a871b72f0ec9d6665a203ea4600bf7
mayack <me@mayack.com>
2025-06-30 21:47:05 +0100

    Visual fixes.

--- constants/config.ts
+++ constants/config.ts
@@ -10,7 +10,7 @@ import type {
 } from '@/types/map'
 
 export const CONFIG = {
-  version: '0.4.3',
+  version: '0.4.4',
   defaultTheme: 'system',
   api: {
     urls: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,7 +57,7 @@ export default {
         sm: 'calc(var(--radius) - 4px)',
       },
       boxShadow: {
-        map: '0 0 1px 1px rgba(0,0,0,0.05), 0 1px 10px rgba(0, 0, 0, 0.15)',
+        map: '0 0 1px 1px rgba(0,0,0,0.1), 0 1px 10px rgba(0, 0, 0, 0.15)',
       },
       fontSize: {
         '2xs': '0.625rem', // 10px
