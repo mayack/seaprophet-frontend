265edf3ca8aa16f6c4c2be961485e63502b7dec1
mayack <me@mayack.com>
2025-12-23 12:36:11 +0000

    Webcam spinner update.

--- components/common/WebcamViewer.tsx
+++ components/common/WebcamViewer.tsx
@@ -203,7 +203,12 @@ export function WebcamViewer({ config }: WebcamViewerProps) {
 
       {status === 'loading' && (
         <Overlay>
-          <Spinner size="lg" className="text-white" />
+          <div className="relative w-full flex justify-center items-center">
+            <Spinner size="lg" className="text-white" />
+            {config.website_url && !streamUrl && (
+              <p className="top-full mt-6 text-sm text-white absolute">This camera takes longer to load.</p>
+            )}
+          </div>
         </Overlay>
       )}
 
