1a9baf033933653bdd40f4f75ef3df7dfcc43788
mayack <me@mayack.com>
2026-05-23 14:10:47 +0100

    Spinner update.

--- app/(authenticated)/spot/[id]/loading.tsx
+++ app/(authenticated)/spot/[id]/loading.tsx
@@ -1,10 +1,10 @@
-import { SpotDetailsSkeleton } from '@/components/spot/SpotsDetails/Skeleton'
+import { Spinner } from '@/components/ui/spinner'
 import React from 'react'
 
 export default function Loading(): React.JSX.Element {
   return (
-    <div className="wrapper-spacing mobile-safe-bottom py-4 sm:py-6 xl:py-8">
-      <SpotDetailsSkeleton />
+    <div className="flex min-h-dvh w-full items-center justify-center bg-background">
+      <Spinner size="lg" />
     </div>
   )
 }
